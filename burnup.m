ff2c16cedef3fe73a555a47a523f93c127b4068b
mishyTheBear <hamish.dickson@gmail.com>
2014-11-13 08:39:50 +0000

    create a training set with time spent and story points

--- burnup.m
+++ burnup.m
@@ -1,3 +1,10 @@
 # Burnup prototype
 
 #TODO .... write this sucker
+
+# first, use just story points vs time taken for each person - then
+# create a graph displaying these for X people, using different colors
+# to show who is who.
+
+# then, given a whole project, find the quickest period of time the
+# project can be completed.

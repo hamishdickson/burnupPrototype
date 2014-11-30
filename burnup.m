28ffe1133870ec35d75794b7cde736b3990e34ab
mishyTheBear <hamish.dickson@gmail.com>
2014-11-30 17:05:56 +0000

    sketch out next steps

--- burnup.m
+++ burnup.m
@@ -37,3 +37,14 @@ hold on;
 plot(X(:,2), X*theta, '-')
 legend('Training data', 'Linear regression')
 hold off;
+
+%-------------------------------------------------------------------------------
+% great, so right now we have trained a function to know what each story point
+% is worth
+% Now, take the given vector of story points and use that to work out how many
+% "person days" are needed to complete the work
+mins_in_day = 60*7; % change this for your business
+
+% be realistic here - what percentage of that time will your team actually code/
+% QA/etc?
+work_factor = 0.7; % I would say that 0.7 may even be a little high
